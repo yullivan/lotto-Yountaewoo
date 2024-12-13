5fe83ad6a20335296bfa6856d0a7b890d1e3fef1
youn <tty7797@naver.com>
2024-12-13 19:06:54 +0900

    feat: Price Class 만들기, 생성자 조건 추가

---
+++ src/main/java/lotto/Price.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import java.util.Objects;
+
+public class Price {
+    int price;
+
+    public Price(int price) {
+        if (price < 1000 || price % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또는  1000원단위입니다.");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(price);
+    }
+}
