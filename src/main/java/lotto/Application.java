b66c45c9842ac9f8558f0b0a812728ba612041e3
youn <tty7797@naver.com>
2024-12-13 19:21:40 +0900

    feat: 구매한금액을 출력하는 함수 추가 & Application 에 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,8 @@ public class Application {
         Output.purchasePrice();
         //구매금액을 입력받기 & 입력받은 금액 Price 에 기억시키기
         Price puchasedPrice = new Price(Input.purchasePrice());
+        //구매한 금액 출력
+        Output.purchasedPrice(puchasedPrice);
 
     }
 
--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -4,5 +4,8 @@ public class Output {
     public static void purchasePrice () {
         System.out.println("구입금액을 입력해 주세요.");
     }
+    public static void purchasedPrice (Price price) {
+        System.out.println(price.getPrice());
+    }
 
 }
